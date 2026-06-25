7c84c268a10a900313dedb3ad8251f5a93cf16c2
Yathurshan Manoharan <prochessgamer01@gmail.com>
2026-06-25 12:32:27 -0400

    feat: add rectangle features

---
+++ Rectangle.java
@@ -0,0 +1,28 @@
+import exercise4.Point;
+
+public class Rectangle {
+    Point A;
+	Point B;
+	Point C;
+	Point D;
+
+	public Rectangle(Point A, Point B, Point C, Point D) {
+        // assuming points A,B,C,D are given in a clockwise order
+		this.A = A;
+		this.B = B;
+		this.C = C;
+        this.D = D;
+
+        if ((A.distance(B) != C.distance(D)) || (B.distance(C) != A.distance(D))) {
+            throw new Exception("Not a valid rectangle");
+        }
+	}
+	
+	public double perimeter() {
+		return 2 * (A.distance(B) + B.distance(C));
+	}
+
+    public double area() {
+		return A.distance(B) * B.distance(C);
+	}
+}
